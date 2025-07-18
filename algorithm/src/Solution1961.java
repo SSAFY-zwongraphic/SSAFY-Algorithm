8600bc5a385146f590a85cd73845eb2db9b4dfcb
admin <admin@naver.com>
2025-07-18 09:03:47 +0900

    0717

---
+++ algorithm/src/Solution1961.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution1961 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 1; t <= T; t++) {
+			int N = Integer.parseInt(br.readLine());
+			int NN[][] = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					NN[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			
+
+		}
+	}
+}
