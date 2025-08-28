699eaf4a6794881beb3adcf833ba24d018003d04
admin <admin@naver.com>
2025-08-28 09:45:00 +0900

    0827

---
+++ algorithm/src/swea/d_0826/Solution1249_bfs.java
@@ -0,0 +1,71 @@
+package swea.d_0826;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Solution1249_bfs {
+	private static int[] dx = { -1, 1, 0, 0 };
+	private static int[] dy = { 0, 0, -1, 1 };
+	private static int n;
+	private static int[][] map;
+	private static int[][] distance;
+
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("swea/d_0826/input1249.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			n = Integer.parseInt(br.readLine());
+			map = new int[n][n];
+			distance = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				String temp = br.readLine();
+				for (int j = 0; j < n; j++) {
+					map[i][j] = temp.charAt(j) - '0';
+				}
+			}
+			distance = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					distance[i][j] = Integer.MAX_VALUE;
+				}
+			}
+			
+			move();
+
+			System.out.printf("#%d %d\n", t, distance[n - 1][n - 1]);
+		}
+	}
+
+	private static void move() {
+		Queue<Integer[]> queue = new ArrayDeque<>();
+
+		distance[0][0] = 0;
+		queue.offer(new Integer[] { 0, 0 });
+
+		while (!queue.isEmpty()) {
+			Integer[] xy = queue.poll();
+			int x = xy[0];
+			int y = xy[1];
+
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+
+				if (nx < 0 || nx >= n || ny < 0 || ny >= n)
+					continue;
+
+				int nextDistance = distance[x][y] + map[nx][ny];
+
+				if (distance[nx][ny] > nextDistance) {
+					distance[nx][ny] = nextDistance;
+					queue.offer(new Integer[] { nx, ny });
+				}
+			}
+		}
+	}
+}
--- algorithm/src/swea/d_0826/Solution1249_dfs.java
+++ algorithm/src/swea/d_0826/Solution1249_dfs.java
@@ -5,14 +5,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Solution1249 {
+//최단거리(다익스트라) 문제라 dfs로 푸는 건 비효율적이긴 함
+public class Solution1249_dfs {
 	// 0: 상, 1:하, 2: 좌, 3: 우
 	private static int[] dx = { -1, 1, 0, 0 };
 	private static int[] dy = { 0, 0, -1, 1 };
 	private static int n;
 	private static int[][] map;
 	private static int[][] distance;
-	private static int min;
 
 	public static void main(String[] args) throws IOException {
 		System.setIn(new FileInputStream("swea/d_0826/input1249.txt"));
@@ -28,11 +28,42 @@ public class Solution1249 {
 					map[i][j] = temp.charAt(j) - '0';
 				}
 			}
+			distance = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					distance[i][j] = Integer.MAX_VALUE;
+				}
+			}
 
-			System.out.printf("#%d %d\n", t, t);
+			distance[0][0] = 0;
+			move(0, 0, 0);
+
+			System.out.printf("#%d %d\n", t, distance[n - 1][n - 1]);
 		}
 	}
 
-	private static void dijkstra() {
+	private static void move(int x, int y, int currentDistance) {
+		if (currentDistance > distance[x][y]) {
+			return;
+		}
+		
+		if ((x == n - 1) && (y == n - 1)) {	
+			return;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= n)
+				continue;
+			
+			int nextDistance = currentDistance + map[x][y];
+			
+			if (nextDistance < distance[nx][ny]) {
+				distance[nx][ny] = nextDistance;
+				move(nx, ny, distance[nx][ny]);
+			}
+		}
 	}
 }
