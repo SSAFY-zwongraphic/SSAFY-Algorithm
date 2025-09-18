76d511b7d30ed24f696c8ebd68036583b317e161
admin <admin@naver.com>
2025-09-18 09:06:41 +0900

    0918

---
+++ algorithm/src/baekjoon/d_0916/Solution9095.java
@@ -0,0 +1,26 @@
+package baekjoon.d_0916;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution9095 {
+	public static void main(String[] args) throws Exception, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
+			int N = Integer.parseInt(br.readLine());
+			int dp[] = new int[N + 3];
+
+			dp[1] = 1;
+			dp[2] = 2;
+			dp[3] = 4;
+
+			for (int n = 4; n <= N; n++) {
+				dp[n] = dp[n - 1] + dp[n - 2] + dp[n - 3];
+			}
+
+			System.out.println(dp[N]);
+		}
+	}
+}
